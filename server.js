420e9844fba435367cb39008926052cef821900e
Ishwant Singh <ishwant8@gmail.com>
2019-04-03 00:39:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express'); // importing a CommonJS module
+
+const hubsRouter = require('./hubs/hubs-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/hubs', hubsRouter);
+
+server.get('/', (req, res, next) => {
+  res.send(`
+    <h2>Lambda Hubs API</h2>
+    <p>Welcome to the Lambda Hubs API</p>
+    `);
+});
+
+module.exports = server;
